2b27eb0b53a11eaa0db42da0e3df672ebd671c16
baxtiyar01 <teshayevbaxtiyor@gmail.com>
2021-01-19 02:22:12 +0500

    New

--- server/modules/upload/resolvers.js
+++ server/modules/upload/resolvers.js
@@ -1,4 +1,5 @@
-const { join, parse } = require('path')
+// const { join, parse } = require('path')
+const { createWriteStream } = require('fs')
 const fs = require('fs')
 const ApolloError = require('apollo-server-express')
 const shortid = require('shortid')
@@ -9,19 +10,29 @@ module.exports =  {
     },
     Mutation: {
         uploadFile: async (_, { file }) => {
+            console.log(file)
             try {
-            	const { createReadStream, filename, mimetype, encoding } = await file
-            	console.log(createReadStream)
-				const id = shortid.generate()
-				const stream = createReadStream()
-				const savedFileName = `${id}-${filename.replace(/ /g,'')}`
-				const path = `./uploadedFiles/${savedFileName}`
-				return new Promise( async (resolve, reject) => {
-    				return stream
-      				.pipe(fs.createWriteStream(path))
-      				.on("finish", () => resolve({ path, filename, mimetype, encoding, savedFileName }))
-      				.on("error", reject)
-				})
+            	// const { createReadStream, filename, mimetype, encoding } = await file
+             //    let { ext, name } = parse(filename)
+             //    let stream = createReadStream()
+             //    name = name.replace(/([^a-z0-9]+)/gi, '-').replace(' ', '_')
+             //    let serverFile = join(__dirname, `../../uploadedFiles/${name}${ext}`)
+             //    let writeStream = await createWriteStream(serverFile)
+             //    await stream.pipe(writeStream)
+             //    return 'salom'
+                const { createReadStream, filename, mimetype, encoding } = await file
+                const id = shortid.generate()7
+                const stream = createReadStream()
+                const savedFileName = `${id}-${filename.replace(/ /g,'')}`
+                const path = `./src/uploadedFiles/${savedFileName}`
+                return new Promise( async (resolve, reject) => {
+                    let writePathToDataBase = await productModel.insertProductFile(savedFileName, productId)
+                    return stream
+                      .pipe(fs.createWriteStream(path))
+                      .on("finish", () => resolve({ path, filename, mimetype, encoding, savedFileName }))
+                      .on("error", reject)
+                })
+                console.log( file)
             } catch (err) {
                 throw new Error(err.message);
             }
--- server/server.js
+++ server/server.js
@@ -10,6 +10,22 @@ const app = express()
 const server = new ApolloServer({ modules, })
 server.applyMiddleware({ app, path: '/graphql'})
 
+app.post('/upload', ( req, res) =>{
+    if(req.files === null)
+    {
+        return res.status(400).json({msg:"No file was uploaded"});
+    }
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/server/uploadedFiles/${file.name}`,err =>{
+        if(err){
+            console.log(err);
+            return res.status(500).send(err);
+        }
+        res.json({fileName:file.name,filePath:`/uploads/${file.name}`});
+    })
+})
+
 const httpServer = http.createServer(app)
 
 app.use(express.static('./server/uploadedFiles'))
